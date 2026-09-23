ac700a827d8c7ee988531fc9287e2a14f9a6c37f
Swikriti Poudel <poudelswikriti76@gmail.com>
2026-09-23 17:56:06 +0545

    Work on UI - layout page and login page

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
    <nav className='bg-green-900 text-white flex justify-around items-center h-15 px-4'>
-    <div className="logo cursor-pointer text-lg font-bold">GiveALittle!</div>
-    <ul className='flex gap-10 cursor-pointer font-bold items-center'>
+    <div className="logo cursor-pointer text-lg font-bold flex justify-center items-center gap-2">
+      <img src="/helping.gif" width={30} alt="" />
+      <span>GiveALittle!</span>
+      </div>
+    {/* <ul className='flex gap-10 cursor-pointer font-bold items-center'>
     <li>Home</li>
     <li>About</li>
     <li>Projects</li>
     <li>Login</li>
     <li>Sign Up</li>
-    </ul>
+    </ul> */}
+
+    <div>
+      <Link href={"/login"}>
+      <button className="text-heading bg-gradient-to-r from-green-200 via-green-400 to-green-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 me-2 mb-2 cursor-pointer">Login</button>
+      </Link>
+    </div>
    </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,18 +3,50 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-  <div className="text-white flex flex-col justify-center  items-center h-[44vh] gap-4">
-   <div className="font-bold text-3xl">Give A Little</div>
+  <div className="flex justify-center flex-col gap-4 items-center text-white h-[44vh] px-5 md:px-0 text-xs md:text-base  ">
+   <div className="font-bold text-5xl flex gap-2 items-center">Give A Little <span><img src="/helping.gif" width={40} alt="" /></span></div>
    <p>A crowdfunding platform for creators. Get funded by your fans and followers.</p>
    <div>
-    <button type="button" class="text-heading bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 me-2 mb-2">Start Now!</button>
+    <button type="button" className="text-heading bg-gradient-to-r from-green-200 via-green-400 to-green-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 me-2 mb-2 cursor-pointer">Start Now!</button>
 
-    <button type="button" class="text-heading bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 me-2 mb-2">Read More</button>
+    <button type="button" className="text-heading bg-gradient-to-r from-green-200 via-green-400 to-green-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 me-2 mb-2 cursor-pointer text">Read More</button>
    </div>
   </div>
-  <div className="bg-white h-1 opacity-10">
-    sdf
+
+
+  <div className="bg-white h-1 opacity-15"></div>
+
+  <div className="text-white container mx-auto pb-32 pt-14">
+    <h2 className="text-3xl font-bold text-center mb-13">Your fans can help you a little.</h2>
+    <div className="flex gap-5 justify-around">
+
+    <div className="item space-y-2 cursor-pointer flex flex-col justify-center items-center">
+    <img src="/person.png" width={100} alt="" className="p-2 rounded-full bg-slate-400"/>
+     <p className="font-bold">Fans want to help you</p>
+    <p className="text-center">Your fans are avaialble for you to help you.</p>
+    </div>
+
+    <div className="item space-y-2 cursor-pointer flex flex-col justify-center items-center">
+    <img src="/fandom.png" width={100} alt="" className="p-2  rounded-full bg-slate-400"/>
+    <p className="font-bold">Fans want to help you</p>
+    <p className="text-center">Your fans are avaialble for you to help you.</p>
+    </div>
+
+    <div className="item space-y-2 cursor-pointer flex flex-col justify-center items-center">
+    <img src="/money.png" width={100} alt="" className="p-2  rounded-full bg-slate-400"/>
+    <p className="font-bold">Fans want to help you</p>
+    <p className="text-center">Your fans are avaialble for you to help you.</p>
+    </div>
+
+    </div>
   </div>
+
+  <div className="bg-white h-1 opacity-15"></div>
+  <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center">
+    <h2 className="text-3xl font-bold text-center mb-13">Your fans can help you a little.</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=XQ_O8CLkJNhiHe8t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+
   </>
   );
 }
